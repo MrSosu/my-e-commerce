7cfebb78ae4831883580748c3b857b9b9edd4d22
m_adriani <m.adriani@jdk.it>
2024-11-11 15:09:45 +0100

    gateway service

--- product-service/src/main/java/com/example/products/product/ProductController.java
+++ product-service/src/main/java/com/example/products/product/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/products")
+@RequestMapping("/app/v1/products")
 public class ProductController {
 
     @Autowired
